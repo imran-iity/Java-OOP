0a8f7e663879f695b721fdbe12f8dec201f04dc2
imran-iity <ih1934037@gmail.com>
2025-07-02 22:40:52 +0600

    Add files via upload

---
+++ Dialog_1.java
@@ -0,0 +1,7 @@
+//3.6
+import javax.swing.JOptionPane;
+public class Dialog_1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to java");
+    }
+}
